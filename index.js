57e33f197cfea9f96a3ade3e0e8f6f0878becc00
abhishek laptop <abhishekchetri@outlook.com>
2019-07-02 02:32:24 +1000

    authentication completed

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+require('./models/user');
 require('./services/passport');
 const authRoutes = require('./routes/authRoutes');
+const keys = require('./config/keys');
+
+mongoose.connect(keys.mongoURI, {dbName: 'emaily'}).then (() =>{
+    console.log('Successful')
+}).catch((err)=> console.log(err));
 
 const app = express();
+
+app.use(cookieSession({
+    maxAge: 30*24*60*60*1000,
+    keys: [keys.cookieKey]
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 authRoutes(app);
 
--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,19 @@
 const passport = require('passport');
 const googlestrategy = require('passport-google-oauth20').Strategy;
 const keys = require('../config/keys');
+const mongoose = require('mongoose');
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done)=> {
+    done(null, user.id);
+})
+
+passport.deserializeUser((id, done)=>{
+    User.findById(id).then(user=>{
+        done(null, user)
+    });
+});
 
 passport.use(
     new googlestrategy({
@@ -9,9 +22,15 @@ passport.use(
         callbackURL:'/auth/google/callback'
     }, 
     (accessToken, refreshToken, profile, done)=>{
-        console.log('access token', accessToken);
-        console.log('refresh token', refreshToken);
-        console.log('profile', profile);
+        User.findOne({googleID: profile.id}).then((exitingUser)=>{
+            if(exitingUser){
+                done(null, exitingUser);
+            }
+            else {
+                new User({googleID: profile.id}).save().then(user=> {
+                    done(null,user)
+                });
+            }
+        })
     })
 );
-
